c10065cb819eb189c0ffc1651e8440f5e7301790
sohrabtowfighi <sohrabtowfighi@hotmail.com>
2019-08-07 10:19:28 -0400

    Update evaluate_SRURGS_on_benchmarks.py

--- evaluate_SRURGS_on_benchmarks.py
+++ evaluate_SRURGS_on_benchmarks.py
@@ -13,40 +13,25 @@ except ImportError:
 
 SR_config1, SR_config2 = pySRURGS.generate_benchmarks_SRconfigs()
 iters = 1000 
-# first twenty problems
-funcs_arity_two = ','.join(SR_config1._n_functions)
-funcs_arity_one = ','.join(SR_config1._f_functions)
-max_num_fit_params = SR_config1._max_num_fit_params
-max_size_trees = SR_config1._max_size_trees
-for z in range(0,20):
-    print("Experiment number:", z)
-    train = './benchmarks/'+str(z)+'_train.csv'
-    for j in range(0,10):
-        print("Run number:", j)
-        run_ID = str(j)        
-        sh.python('pySRURGS.py',
+
+def run_experiments(SRconfig, start_index, count_experiments, n_runs):
+    funcs_arity_two = ','.join(SR_config._n_functions)
+    funcs_arity_one = ','.join(SR_config._f_functions)
+    max_num_fit_params = SR_config._max_num_fit_params
+    max_size_trees = SR_config._max_size_trees
+    # first twenty problems
+    for z in range(start_index,count_experiments):
+        print("Experiment number:", z)
+        train = './benchmarks/'+str(z)+'_train.csv'
+        for j in range(0,n_runs):
+            print("Run number:", j)
+            run_ID = str(j)        
+            sh.python('pySRURGS.py',
                   '-run_ID', run_ID,
                   '-funcs_arity_two', funcs_arity_two,
                   '-max_num_fit_params', max_num_fit_params,
                   '-max_size_trees', max_size_trees,
                   train, iters)
 
-# remaining 80 cases 
-# first twenty problems
-funcs_arity_two = ','.join(SR_config2._n_functions)
-funcs_arity_one = ','.join(SR_config2._f_functions)
-max_num_fit_params = SR_config2._max_num_fit_params
-max_size_trees = SR_config2._max_size_trees
-for z in range(20,99):
-    print("Experiment number:", z)
-    train = './benchmarks/'+str(z)+'_train.csv'    
-    for j in range(0,10):
-        print("Run number:", j)
-        run_ID = str(j)
-        sh.python('pySRURGS.py', 
-                  '-run_ID', run_ID,
-                  '-funcs_arity_two', funcs_arity_two,
-                  '-funcs_arity_one', funcs_arity_one, 
-                  '-max_num_fit_params', max_num_fit_params,
-                  '-max_size_trees', max_size_trees,
-                  train, iters)
+run_experiments(SR_config1, 0, 20)
+run_experiments(SR_config1, 20, 80)
