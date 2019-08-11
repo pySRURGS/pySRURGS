ed158067be6e1450b13380778e6b52265f05f260
sohrabtowfighi <sohrabtowfighi@hotmail.com>
2019-08-11 05:59:00 -0400

    Delete bash_command.sh
    Former-commit-id: 908073ffead601db06d91e070b305aaa1f176cc3 [formerly 89927184eb59ab5fe332e29986f9877bcd238a62]
    Former-commit-id: 464c8ac15ce1051226b35ec3338d7bf514903e34
    Former-commit-id: 8d023a3f914eefe4c97635a056c9119eee6ef6f4

--- bash_command.sh
+++
@@ -1,7 +0,0 @@
-EXP_PER_RUN=10
-let "TIME_PER_RUN=$EXP_PER_RUN * 3"
-while [[ 1 -eq 1 ]]
-do
-    timeout ${TIME_PER_RUN}s winpty python pySRURGS.py ./csvs/x1_squared_minus_five_x3.csv $EXP_PER_RUN single_process
-done
-    
