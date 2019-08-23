c69547ce08001a4745f4a8c9a55a5b1e8933e306
sohrabtowfighi <sohrabtowfighi@hotmail.com>
2019-08-22 23:31:32 -0400

    fixed major issue in SRGP to SRURGS comparison but need to test it
    Former-commit-id: ec13da5d6d48ae77224b03b65299fb786d37fc51

--- experiments/assess_stochasticity_of_SRURGS_space.py
+++ experiments/assess_stochasticity_of_SRURGS_space.py
@@ -7,10 +7,16 @@ import pySRURGS
 import pdb
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+from sqlitedict import SqliteDict
 from matplotlib.ticker import MaxNLocator
+import os
+import seaborn as sns
 
-def main(max_permitted_trees, max_num_fit_params, i, r0, s0):
+path_to_db = './stochasticity.db'
+
+def main(max_permitted_trees, max_num_fit_params, i, r0, s0, figpath):
     SR_config = pySRURGS.SymbolicRegressionConfig(['add','sub','mul','div','pow'],
                                         [],
                                         max_num_fit_params=max_num_fit_params,
@@ -25,57 +31,44 @@ def main(max_permitted_trees, max_num_fit_params, i, r0, s0):
     rows = 125
     cols = 81
     data = np.ones((rows,cols))
+    x_data = np.ones((rows,cols))
+    y_data = np.ones((rows,cols))
     print(A)
     print(B)
-    x_data = []
-    y_data = []
-    for r in range(r0, r0+rows):
-        print(r, A)        
-        for s in range(s0, s0+cols):           
-            try:
-                eqn = pySRURGS.equation_generator2(i, r, s, dataset, en, SR_config, simpler=False)
-            except:
-                pdb.set_trace()
-            eqn = pySRURGS.clean_funcstring(eqn)
-            try:
-                (sum_of_squared_residuals, 
-                sum_of_squared_totals, 
-                R2,
-                params_dict_to_store, 
-                residual) = pySRURGS.check_goodness_of_fit(eqn, params, dataset)
-            except FloatingPointError:
-                pass            
-            if R2 > 1 or R2 < 0:
-                R2 = 0
-            data[r-r0,s-s0] = R2
-            x_data.append(r)
-            y_data.append(s)
-        
-
-
-    fig = plt.figure()
-    ax = fig.gca()
+    with SqliteDict(path_to_db, autocommit=True) as results_dict:
+        for r in range(r0, r0+rows):
+            print(r, A)
+            for s in range(s0, s0+cols):            
+                try:
+                    R2 = results_dict[str(i)+str(r)+str(s)]
+                except KeyError:                               
+                    try:
+                        eqn = pySRURGS.equation_generator2(i, r, s, dataset, en, SR_config, simpler=False) 
+                        eqn = pySRURGS.clean_funcstring(eqn) 
+                        (sum_of_squared_residuals, 
+                        sum_of_squared_totals, 
+                        R2,
+                        params_dict_to_store, 
+                        residual) = pySRURGS.check_goodness_of_fit(eqn, params, dataset)
+                    except FloatingPointError:
+                        pass            
+                    results_dict[str(i)+str(r)+str(s)] = R2
+                if R2 > 1 or R2 < 0:
+                    R2 = 0
+                data[r-r0,s-s0] = R2
+                x_data[r-r0,s-s0] = r
+                y_data[r-r0,s-s0] = s
+    fig = plt.figure(figsize=(3.14, 2))
+    ax = sns.heatmap(data, vmin=0, vmax=1, linewidth=0.0, cmap=matplotlib.cm.viridis, yticklabels=False, xticklabels=False, cbar_kws={'label':'R^2', 'ticks':[0, 0.5, 1]})
     ax.yaxis.set_major_locator(MaxNLocator(integer=True))
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    scat = ax.scatter(x_data, y_data, c=data.flatten())
-    cbar = plt.colorbar(scat, ax=ax, boundaries=np.linspace(0,1,100))
-    cbar.set_label('R^2')
     plt.xlabel("r")
     plt.ylabel("s")
     plt.tight_layout()
     plt.show()
-    plt.clf()
+    plt.savefig(figpath, bbox_inches='tight')    
 
-if __name__ == '__main__':
-    main(10, 2, 3, 0, 0)
-    main(100, 2, 50, 1000, 1000)
-'''
-x_data = list(range(0,A))
-y_data = list(range(0,B))
-plt.contour(y_data, x_data, data, cmap='RdGy')
-cbar = plt.colorbar()
-cbar.set_label('R^2')
-ax = fig.gca()
-ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-plt.show()'''
+if __name__ == '__main__':    
+    figpath = 'C:/Users/sohra/Google Drive (fischerproject2018@gmail.com)/pySRURGS/experiments/figures/Fig2.png'
+    main(10, 2, 3, 0, 0, figpath)
+    
