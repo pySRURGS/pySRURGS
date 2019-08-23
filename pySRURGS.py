e80586e3b6018d6bfc9455cafa904a47224b52be
sohrabtowfighi <sohrabtowfighi@hotmail.com>
2019-08-23 15:43:16 -0400

    redid the test script so that i call the python code itself in addition to CLI
    Former-commit-id: 1107cdf5a79c4bd1864c562a749a4e08fad6aaa0 [formerly 0d60c94c01abd458f20ba328cbd1237b9e82294b]
    Former-commit-id: a9cbbe178a2ddb4f9e014fc2ed8121f6b8c9547a
    Former-commit-id: cfcfba48b94072dfc21c6c94d539daf369a06d2b

--- pySRURGS.py
+++ pySRURGS.py
@@ -61,6 +61,10 @@ benchmarks_fit_param_domain = [-10, 10]
 benchmarks_dir = './csvs/benchmarks'
 benchmarks_summary_tsv = './benchmarks_summary.tsv'
 memoize_funcs = False
+defaults_dict = {'funcs_arity_two': 'add,sub,mul,div,pow',
+                 'max_num_fit_params': 3,
+                 'max_permitted_trees':1000,
+                 'path_to_db': None}
 ##### END GLOBALS
 
 def memoize(func):
@@ -1218,11 +1222,11 @@ if __name__ == '__main__':
     parser.add_argument("-count", help="Instead of doing symbolic regression, just count out how many possible equations for this configuration. No other processing performed.", action="store_true")
     parser.add_argument("-benchmarks", help="Instead of doing symbolic regression, generate the 100 benchmark problems. No other processing performed.", action="store_true")
     parser.add_argument("-plotting", help="plot the best model against the data to ./image/plot.png and ./image/plot.svg - note only works for univariate datasets", action="store_true")
-    parser.add_argument("-funcs_arity_two", help="a comma separated string listing the functions of arity two you want to be considered. Permitted:add,sub,mul,div,pow", default='add,sub,mul,div,pow')
+    parser.add_argument("-funcs_arity_two", help="a comma separated string listing the functions of arity two you want to be considered. Permitted:add,sub,mul,div,pow", default=defaults_dict['funcs_arity_two'])
     parser.add_argument("-funcs_arity_one", help="a comma separated string listing the functions of arity one you want to be considered. Permitted:sin,cos,tan,exp,log,sinh,cosh,tanh")
-    parser.add_argument("-max_num_fit_params", help="the maximum number of fitting parameters permitted in the generated models", default=3, type=int)
-    parser.add_argument("-max_permitted_trees", help="the number of unique binary trees that are permitted in the generated models - binary trees define the form of the equation, increasing this number tends to increase the complexity of generated equations", default=1000, type=int)
-    parser.add_argument("-path_to_db", help="the absolute or relative path to the database file where we will save results", default=None)
+    parser.add_argument("-max_num_fit_params", help="the maximum number of fitting parameters permitted in the generated models", default=defaults_dict['max_num_fit_params'], type=int)
+    parser.add_argument("-max_permitted_trees", help="the number of unique binary trees that are permitted in the generated models - binary trees define the form of the equation, increasing this number tends to increase the complexity of generated equations", default=defaults_dict['max_permitted_trees'], type=int)
+    parser.add_argument("-path_to_db", help="the absolute or relative path to the database file where we will save results", default=defaults_dict['path_to_db'])
     if len(sys.argv) < 2:
         parser.print_usage()
         sys.exit(1)
