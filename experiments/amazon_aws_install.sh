31c0a20d5c720c4643ecdac6a95e161e2c05492f
sohrabtowfighi <sohrabtowfighi@hotmail.com>
2019-08-28 18:57:04 -0400

    jobs are distributed to cloud computing
    Former-commit-id: fa797c1967721aee7266944c68777b93a5d33126

--- experiments/amazon_aws_install.sh
+++ experiments/amazon_aws_install.sh
@@ -1,10 +1,11 @@
 sudo apt-get update
 sudo apt-get install -y python3-pip
 sudo cp /usr/bin/python3 /usr/bin/python
-
+sudo apt-get install -y git vim 
 cd ~
 git clone https://github.com/pySRURGS/pySRURGS.git
 cd pySRURGS
 pip3 install -r requirements.txt --user
 cd experiments
+pip3 install -r requirements.txt --user
 vim secret.py
