ca7db9d975c00277d66af431544c0bb663ef9be1
sohrabtowfighi <sohrabtowfighi@hotmail.com>
2019-11-17 23:30:21 -0500

    added rightmost column note to pySRURGS -h output, time complexity script added code
    Former-commit-id: 6ebfa0919848552033cba60f4dd6f8945322ca18 [formerly c8cf8f203c05e78ed253a47d794f27a9c1a053a8]
    Former-commit-id: d560c2ab7aa3cfe3bf7e6215224ead07a4ee77da
    Former-commit-id: f6cdfd7866370c1f0b9dc34dbc2ef87363e6c37c

--- run_tests_for_time_complexity_assessment.py
+++ run_tests_for_time_complexity_assessment.py
@@ -1,13 +1,34 @@
+# this script needs to be run in the same directory as pySRURGS.py
 import os 
 import sys
 import time
 import pySRURGS
+try:
+    import sh
+except ImportError:
+    # fallback: emulate the sh API with pbs
+    import pbs
+
+    class Sh(object):
+        def __getattr__(self, attr):
+            return pbs.Command(attr)
+    sh = Sh()
+    
+def refresh_db(path_to_db):
+    try:
+        os.remove(path_to_db)
+    except:
+        pass
+
 num_iters_in_test = 100
 n_funcs = ['add','sub','mul','div']
 f_funcs = []
 n_par = 2
 benchmark_name = str(1)
-for n_tree in [10, 100, 1000]:
+qrtic_polynml_csv = './csv/quartic_polynomial.csv'
+qrtic_polynml_db = './db/quartic_polynomial.db'
+'''
+for n_tree in [10, 100, 1000, 10000]:
     SR_config = pySRURGS.SymbolicRegressionConfig(n_funcs, f_funcs, n_par, n_tree)
     init_time = time.time()
     for i in range(0, num_iters_in_test):
@@ -15,3 +36,14 @@ for n_tree in [10, 100, 1000]:
     end_time = time.time()
     diff_time = end_time - init_time 
     print("n_tree", n_tree, "time", diff_time)
+'''
+for n_tree in [10, 100, 1000, 10000]:
+    refresh_db(qrtic_polynml_db)
+    SR_config = pySRURGS.SymbolicRegressionConfig(n_funcs, f_funcs, n_par, n_tree)
+    init_time = time.time()
+    for i in range(0, num_iters_in_test):
+        pySRURGS.uniform_random_global_search_once(qrtic_polynml_db, qrtic_polynml_csv, SR_config)
+    end_time = time.time()
+    diff_time = end_time - init_time 
+    print("n_tree", n_tree, "time", diff_time)
+    
