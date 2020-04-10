500a34c68b7e553f089bd4e7b4b396cc32e02a9e
sohrabtowfighi <sohrabtowfighi@hotmail.com>
2020-04-10 11:21:22 -0700

    making it such that only one process writes to disk
    Former-commit-id: 6ccd82c825d3e24e447b90bcb1dd254f46ee7fd8 [formerly f7dfa148cdd280adec4193a34fc89030c1050401]
    Former-commit-id: 44e3fe72ab3218f0492e2883590eb1ef5c6b10ed
    Former-commit-id: e3fabef674555a46acec310956f3c334a50d6789

--- test.py
+++ test.py
@@ -38,108 +38,110 @@ def refresh_db(path_to_db):
     except:
         pass
 
-'''
-class TestCommandLineInterface(unittest.TestCase):
-    
-    def setUp(self):
-        refresh_db(working_db)
+test_CLI = False
 
-    def test_single_processor_deterministic(self):        
-        output = sh.python('pySRURGS.py', '-single', '-deterministic', 
-                           '-path_to_db', working_db, qrtic_polynml_csv, 
-                           num_iters)
-        output = output.strip()
-        print(output)
-        n_results = pySRURGS.count_results(working_db)        
-        self.assertGreater(n_results, 0.95*num_iters)
+if test_CLI == True:
+    class TestCommandLineInterface(unittest.TestCase):
         
-    def test_zero_fit_params(self):
-        output = sh.python('pySRURGS.py', '-deterministic', 
-                           '-max_num_fit_params', 0, '-path_to_db', working_db, 
-                           qrtic_polynml_csv, num_iters)
-        output = output.strip()
-        print(output)
-        result_list = pySRURGS.get_resultlist(working_db)
-        n_results = pySRURGS.count_results(working_db)
-        self.assertGreater(n_results, 0.9*num_iters)
-        for i in range(0, n_results):
-            self.assertEqual(len(result_list._results[i]._params), 0)
-        
-    def test_five_fit_params(self):
-        output = sh.python('pySRURGS.py', '-deterministic', 
-                           '-max_num_fit_params', 5, '-path_to_db', working_db, 
-                           qrtic_polynml_csv, num_iters)
-        output = output.strip()
-        print(output)
-        n_results = pySRURGS.count_results(working_db)
-        self.assertGreater(n_results, 0.98*num_iters)
+        def setUp(self):
+            refresh_db(working_db)
 
-    def test_funcs_arity_two(self):
-        output = output = sh.python('pySRURGS.py', '-deterministic', 
-                                    '-funcs_arity_two', 'add,sub,div', 
-                                    '-max_num_fit_params', 5, '-path_to_db', 
-                                    working_db, qrtic_polynml_csv, num_iters)
-        output = output.strip()
-        print(output)
-        n_results = pySRURGS.count_results(working_db)
-        result_list = pySRURGS.get_resultlist(working_db)
-        self.assertGreater(n_results, 0.98*num_iters)
-        for i in range(0, n_results):
-            my_result = result_list._results[i]
-            simple_eqn = my_result._simple_equation
-            self.assertEqual(('mul' in simple_eqn), False)
-            self.assertEqual(('exp' in simple_eqn), False)
+        def test_single_processor_deterministic(self):        
+            output = sh.python('pySRURGS.py', '-single', '-deterministic', 
+                               '-path_to_db', working_db, qrtic_polynml_csv, 
+                               num_iters)
+            output = output.strip()
+            print(output)
+            n_results = pySRURGS.count_results(working_db)        
+            self.assertGreater(n_results, 0.95*num_iters)
+            
+        def test_zero_fit_params(self):
+            output = sh.python('pySRURGS.py', '-deterministic', 
+                               '-max_num_fit_params', 0, '-path_to_db', working_db, 
+                               qrtic_polynml_csv, num_iters)
+            output = output.strip()
+            print(output)
+            result_list = pySRURGS.get_resultlist(working_db)
+            n_results = pySRURGS.count_results(working_db)
+            self.assertGreater(n_results, 0.9*num_iters)
+            for i in range(0, n_results):
+                self.assertEqual(len(result_list._results[i]._params), 0)
+            
+        def test_five_fit_params(self):
+            output = sh.python('pySRURGS.py', '-deterministic', 
+                               '-max_num_fit_params', 5, '-path_to_db', working_db, 
+                               qrtic_polynml_csv, num_iters)
+            output = output.strip()
+            print(output)
+            n_results = pySRURGS.count_results(working_db)
+            self.assertGreater(n_results, 0.98*num_iters)
 
-    def test_funcs_arity_one(self):
-        output = sh.python('pySRURGS.py', '-deterministic', '-funcs_arity_one', 
-                           'tan,exp', '-max_num_fit_params', 5, '-path_to_db', 
-                            working_db, qrtic_polynml_csv, num_iters)
-        output = output.strip()
-        print(output)
-        n_results = pySRURGS.count_results(working_db)
-        result_list = pySRURGS.get_resultlist(working_db)
-        self.assertGreater(n_results, 0.98*num_iters)
-        for i in range(0, n_results):
-            my_result = result_list._results[i]
-            simple_eqn = my_result._simple_equation
-            self.assertEqual(('cos' in simple_eqn), False)
+        def test_funcs_arity_two(self):
+            output = output = sh.python('pySRURGS.py', '-deterministic', 
+                                        '-funcs_arity_two', 'add,sub,div', 
+                                        '-max_num_fit_params', 5, '-path_to_db', 
+                                        working_db, qrtic_polynml_csv, num_iters)
+            output = output.strip()
+            print(output)
+            n_results = pySRURGS.count_results(working_db)
+            result_list = pySRURGS.get_resultlist(working_db)
+            self.assertGreater(n_results, 0.98*num_iters)
+            for i in range(0, n_results):
+                my_result = result_list._results[i]
+                simple_eqn = my_result._simple_equation
+                self.assertEqual(('mul' in simple_eqn), False)
+                self.assertEqual(('exp' in simple_eqn), False)
 
-    def test_max_permitted_trees(self):
-        output = sh.python('pySRURGS.py', '-deterministic', 
-                           '-max_permitted_trees', 10, '-max_num_fit_params', 5, 
-                           '-path_to_db', working_db, qrtic_polynml_csv, 
-                           num_iters)
-        output = output.strip()
-        print(output)
-        n_results = pySRURGS.count_results(working_db)
-        result_list = pySRURGS.get_resultlist(working_db)
-        self.assertGreater(n_results, 0.98*num_iters)    
+        def test_funcs_arity_one(self):
+            output = sh.python('pySRURGS.py', '-deterministic', '-funcs_arity_one', 
+                               'tan,exp', '-max_num_fit_params', 5, '-path_to_db', 
+                                working_db, qrtic_polynml_csv, num_iters)
+            output = output.strip()
+            print(output)
+            n_results = pySRURGS.count_results(working_db)
+            result_list = pySRURGS.get_resultlist(working_db)
+            self.assertGreater(n_results, 0.98*num_iters)
+            for i in range(0, n_results):
+                my_result = result_list._results[i]
+                simple_eqn = my_result._simple_equation
+                self.assertEqual(('cos' in simple_eqn), False)
 
-    def test_combined_parameters_1(self):        
-        output = sh.python('pySRURGS.py', '-exhaustive', '-funcs_arity_two', 
-                           'add,sub', '-funcs_arity_one', 'sin', 
-                           '-max_permitted_trees', 3, '-max_num_fit_params', 1, 
-                           '-path_to_db', working_db, qrtic_polynml_csv, 
-                           num_iters)
-        output = output.strip()
-        print(output)
-        self.assertEqual('12.0' in output, True)
-        n_results = pySRURGS.count_results(working_db)
-        result_list = pySRURGS.get_resultlist(working_db)
-        self.assertGreater(n_results, 0.98*num_iters)
+        def test_max_permitted_trees(self):
+            output = sh.python('pySRURGS.py', '-deterministic', 
+                               '-max_permitted_trees', 10, '-max_num_fit_params', 5, 
+                               '-path_to_db', working_db, qrtic_polynml_csv, 
+                               num_iters)
+            output = output.strip()
+            print(output)
+            n_results = pySRURGS.count_results(working_db)
+            result_list = pySRURGS.get_resultlist(working_db)
+            self.assertGreater(n_results, 0.98*num_iters)    
 
-    def test_combined_parameters_2(self):
-        output = sh.python('pySRURGS.py', '-exhaustive', '-funcs_arity_two', 
-                           'add,sub', '-max_permitted_trees', 3, 
-                           '-max_num_fit_params', 1, 
-                           '-path_to_db', working_db, qrtic_polynml_csv, 10)
-        output = output.strip()
-        print(output)
-        self.assertEqual('42.0' in output, True)
-        n_results = pySRURGS.count_results(working_db)
-        result_list = pySRURGS.get_resultlist(working_db)
-        self.assertGreater(n_results, 0.98*num_iters)
-'''
+        def test_combined_parameters_1(self):        
+            output = sh.python('pySRURGS.py', '-exhaustive', '-funcs_arity_two', 
+                               'add,sub', '-funcs_arity_one', 'sin', 
+                               '-max_permitted_trees', 3, '-max_num_fit_params', 1, 
+                               '-path_to_db', working_db, qrtic_polynml_csv, 
+                               num_iters)
+            output = output.strip()
+            print(output)
+            self.assertEqual('12.0' in output, True)
+            n_results = pySRURGS.count_results(working_db)
+            result_list = pySRURGS.get_resultlist(working_db)
+            self.assertGreater(n_results, 0.98*num_iters)
+
+        def test_combined_parameters_2(self):
+            output = sh.python('pySRURGS.py', '-exhaustive', '-funcs_arity_two', 
+                               'add,sub', '-max_permitted_trees', 3, 
+                               '-max_num_fit_params', 1, 
+                               '-path_to_db', working_db, qrtic_polynml_csv, 10)
+            output = output.strip()
+            print(output)
+            self.assertEqual('42.0' in output, True)
+            n_results = pySRURGS.count_results(working_db)
+            result_list = pySRURGS.get_resultlist(working_db)
+            self.assertGreater(n_results, 0.98*num_iters)
+
 
 def test_python_code():
     print('Started run_python_tests')
@@ -177,7 +179,7 @@ def test_python_code():
     max_attempts = 15
     # test the basic functionality
     for i in range(0, max_attempts):
-        pySRURGS.uniform_random_global_search_once(None,
+        pySRURGS.uniform_random_global_search_once_to_db(None,
             path_to_db, path_to_csv, SRconfig)
     assert type(num_equations) == mpmath.ctx_mp_python.mpf
     print(num_equations)
@@ -191,7 +193,7 @@ def test_python_code():
     # test the multiprocessing functionality and that MSE decreases with 1000 runs
     max_attempts = 100
     for i in tqdm.tqdm(range(0, max_attempts)):
-        pySRURGS.uniform_random_global_search_once(None,
+        pySRURGS.uniform_random_global_search_once_to_db(None,
             path_to_db, path_to_csv, SRconfig)
     result_list = pySRURGS.compile_results(path_to_db, path_to_csv, SRconfig)
     MSE_2nd_run = result_list._results[0]._MSE
@@ -206,13 +208,13 @@ def test_python_code():
     SRconfig = pySRURGS.SymbolicRegressionConfig(
         n_funcs, f_funcs, 0, max_permitted_trees)
     for i in tqdm.tqdm(range(0, max_attempts)):
-        pySRURGS.uniform_random_global_search_once(None,
+        pySRURGS.uniform_random_global_search_once_to_db(None,
             path_to_db, path_to_csv, SRconfig)
     # test max_num_fit_params 5
     SRconfig = pySRURGS.SymbolicRegressionConfig(
         n_funcs, f_funcs, 5, max_permitted_trees)
     for i in tqdm.tqdm(range(0, max_attempts)):
-        pySRURGS.uniform_random_global_search_once(None,
+        pySRURGS.uniform_random_global_search_once_to_db(None,
             path_to_db, path_to_csv, SRconfig)
     # test funcs_arity_two = 'add,sub,div'
     test_n_funcs = 'add,sub,div'
@@ -221,26 +223,25 @@ def test_python_code():
     SRconfig_test_n_funcs = pySRURGS.SymbolicRegressionConfig(
         test_n_funcs, f_funcs, 5, max_permitted_trees)
     for i in tqdm.tqdm(range(0, max_attempts)):
-        pySRURGS.uniform_random_global_search_once(None,
+        pySRURGS.uniform_random_global_search_once_to_db(None,
             path_to_db, path_to_csv, SRconfig)
     # test funcs_arity_one = 'tan,exp'
     SRconfig_test_f_funcs = pySRURGS.SymbolicRegressionConfig(
         n_funcs, test_f_funcs, 5, max_permitted_trees)
     for i in tqdm.tqdm(range(0, max_attempts)):
-        pySRURGS.uniform_random_global_search_once(None,
+        pySRURGS.uniform_random_global_search_once_to_db(None,
             path_to_db, path_to_csv, SRconfig_test_f_funcs)
     # test max_permitted_trees = 10
     test_max_permitted_trees = 10
     SRconfig_test_permitted_trees = pySRURGS.SymbolicRegressionConfig(
         n_funcs, f_funcs, 5, test_max_permitted_trees)
     for i in tqdm.tqdm(range(0, max_attempts)):
-        pySRURGS.uniform_random_global_search_once(None,
+        pySRURGS.uniform_random_global_search_once_to_db(None,
             path_to_db, path_to_csv, SRconfig_test_permitted_trees)
     # plot results
     pySRURGS.plot_results(path_to_db, path_to_csv, SRconfig)
     # generate benchmarks
-    pySRURGS.generate_benchmarks()
-    print('Finished run_python_tests')
+    pySRURGS.generate_benchmarks()    
     # print DB inspection code
     SR_config = pySRURGS.SymbolicRegressionConfig()
     path_to_csv = './csv/quartic_polynomial.csv'
@@ -258,13 +259,15 @@ def test_python_code():
     # run tests for exhaustive search
     refresh_db(path_to_db)
     SRconfig = pySRURGS.SymbolicRegressionConfig(['add', 'sub'], ['sin'], 1, 3)
-    pySRURGS.exhaustive_search(path_to_db, path_to_csv, SRconfig, mode='multi')
+    pySRURGS.exhaustive_search(path_to_db, path_to_csv, SRconfig)
     refresh_db(path_to_db)
     SRconfig = pySRURGS.SymbolicRegressionConfig(['add', 'sub'], [], 1, 3)
     pySRURGS.exhaustive_search(
-        path_to_db, path_to_csv, SRconfig, mode='single')
+        path_to_db, path_to_csv, SRconfig)
     refresh_db(path_to_db)
+    print('Finished run_python_tests')
 
 if __name__ == '__main__':
-    test_python_code()
-    #unittest.main(verbosity=2) # this test does not yet work on travis-CI owing to NFS filesystem  
+    test_python_code()    
+    if test_CLI == True:
+        unittest.main(verbosity=2) # this test does not yet work on travis-CI owing to NFS filesystem  
